e6c3589b3a38aa3528b557240e20e85b55a6f4d1
breakthestatic <breakthestatic@gmail.com>
2024-02-29 13:53:15 -0800

    Optimize background animation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import {createRoot} from 'react-dom/client'
 import routes from '/src/routes'
 
-document.body.innerHTML = '<div id="app"></div>'
-
 const root = createRoot(document.getElementById('app'))
 root.render(<RouterProvider router={createBrowserRouter(routes)} />)
